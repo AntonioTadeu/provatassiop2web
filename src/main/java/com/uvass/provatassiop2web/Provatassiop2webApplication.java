217316d570d012d412de8a382e88171cce5d8d72
AntonioTadeu <antoniotadeubf98@gmail.com>
2021-11-20 04:06:25 -0300

    Feat: criação de estrutura de projetos
    Foi feito a estruturação do projeto.

---
+++ src/main/java/com/uvass/provatassiop2web/Provatassiop2webApplication.java
@@ -0,0 +1,13 @@
+package com.uvass.provatassiop2web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provatassiop2webApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Provatassiop2webApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uvass/provatassiop2web/Provatassiop2webApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvass.provatassiop2web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provatassiop2webApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
