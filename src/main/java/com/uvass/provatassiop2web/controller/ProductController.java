b6471cc5a5114deecfcd407eb0d7c89d4bdb5026
AntonioTadeu <antoniotadeubf98@gmail.com>
2021-11-20 19:58:31 -0300

    Feat: Criação do controller
    Foi criado o controller.

---
+++ src/main/java/com/uvass/provatassiop2web/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.uvass.provatassiop2web.controller;
+
+import com.uvass.provatassiop2web.model.entity.Product;
+import com.uvass.provatassiop2web.model.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
+public class ProductController {
+    private ProductRepository productRepository;
+
+    public ProductController(JdbcTemplate jdbcTemplate) {
+        productRepository = new ProductRepository(jdbcTemplate);
+    }
+
+    @CrossOrigin("*")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping()
+    public List<Product> search() throws Exception {
+        return productRepository.search();
+    }
+}
